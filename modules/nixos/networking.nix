fc1e097178a49217ab201ee8c46d178952c73221
Dima Kharitonov <mail@dima.pub>
2026-03-15 21:44:19 +0100

    improved dns

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,6 +1,18 @@
-{ ... }:
+{ secrets, ... }:
 
 {
+  services.resolved = {
+    enable = true;
+    dnsovertls = "true";
+    domains = [ "~." ];
+    extraConfig = ''
+      DNS=45.90.28.0#${secrets.nextdnsId}.dns.nextdns.io
+      DNS=2a07:a8c0::#${secrets.nextdnsId}.dns.nextdns.io
+      DNS=45.90.30.0#${secrets.nextdnsId}.dns.nextdns.io
+      DNS=2a07:a8c1::#${secrets.nextdnsId}.dns.nextdns.io
+    '';
+  };
+
   services.avahi = {
     enable = true;
     nssmdns4 = true;
@@ -11,6 +23,7 @@
     wireless.enable = false;
     networkmanager = {
       enable = true;
+      dns = "systemd-resolved";
       ensureProfiles = {
         profiles = {
           "USB-Ethernet" = {
