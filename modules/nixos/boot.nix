7ef60006fade8128b0df7e569616f4d20b8a0079
Dima Kharitonov <mail@dima.pub>
2026-03-15 21:43:40 +0100

    support secure boot

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,10 +1,15 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
+  environment.systemPackages = [ pkgs.sbctl ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "uinput" ];
-  boot.kernelParams = [ "amdgpu.dcdebugmask=0x10" ];
+  boot.kernelParams = [ ];
   boot.initrd.luks.devices."luks-cfffc0b1-1359-4158-b88c-53d4ebbc5a54".device = "/dev/disk/by-uuid/cfffc0b1-1359-4158-b88c-53d4ebbc5a54";
 }
